796134bf78fb54c96d78ff3eb6525009a6ab08c6
Andrew ONeal <oneal.andrew@gmail.com>
2013-12-11 21:59:13 -0800

    404

---
+++ wp-content/themes/new-proxy-child-theme/404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class='hfeed'>
+
+    <article id="post" >
+
+        <h2 class="entry-title">Epic 404! </h2>
+
+        <div class="entry-content clearfix">
+            <p>Sorry, we couldn't find the page you're looking for. Head back <a href="http://filmbundle.com">home</a> or <a href="mailto:hq@filmbundle.com">contact us</a> if this is reached in error.
+        </div>
+
+    </article>
+
+</div>
+
+<?php get_footer() ?>

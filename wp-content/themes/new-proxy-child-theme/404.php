4570127618a2f7f890f47c1ad261f71ed6069dae
Andrew ONeal <oneal.andrew@gmail.com>
2013-12-12 14:30:39 -0800

    404 update

--- wp-content/themes/new-proxy-child-theme/404.php
+++ wp-content/themes/new-proxy-child-theme/404.php
@@ -4,10 +4,10 @@
 
     <article id="post" >
 
-        <h2 class="entry-title">Epic 404! </h2>
+        <h2 style="font-size: 60px;text-align: center;margin-top: 50px;" class="entry-title">Epic 404! </h2>
 
         <div class="entry-content clearfix">
-            <p>Sorry, we couldn't find the page you're looking for. Head back <a href="http://filmbundle.com">home</a> or <a href="mailto:hq@filmbundle.com">contact us</a> if this is reached in error.
+            <p style="text-align: center;width: 500px;margin: auto;">Sorry, we couldn't find the page you're looking for. Head back <a href="http://filmbundle.com">home</a> or <a href="mailto:hq@filmbundle.com">contact us</a> if this is reached in error.
         </div>
 
     </article>
